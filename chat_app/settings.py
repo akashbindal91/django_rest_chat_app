56e0ced8dd9811de172d0940e9991f8401961cb2
Akash Bindal <akashbindal91@gmail.com>
2020-09-21 12:10:26 +0530

    user registraiton done and .vscode removed

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts',
+    'rest_framework.authtoken'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
